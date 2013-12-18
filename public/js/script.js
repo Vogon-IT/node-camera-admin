6e60b4de887e55cee5db8324ec3acad6b92a6e49
teemujonkkari <teemu.jonkkari@gmail.com>
2013-12-18 17:37:57 +0200

    Cleanup

--- public/js/script.js
+++ public/js/script.js
@@ -9,28 +9,17 @@ $('#form').on('submit', function(event) {
 
   var form = $(this).serialize();
 
-  console.log(form);
-
   var jqxhr = $.post('/admin', form, function(data) {
-    if(data.status === 1) {
+    if (data.status === 1) {
       $('.alert-success').show();
     } else {
       $('.alert-danger').text(data.message).show();
     }
   })
-  .fail(function() {
-    $('.alert-danger').text('Something went badly wrong while saving the new configuration.').show();
-  })
-  .always(function() {
-    window.scrollTo(0,0);
-  });
-
+    .fail(function() {
+      $('.alert-danger').text('Something went badly wrong while saving the new configuration.').show();
+    })
+    .always(function() {
+      window.scrollTo(0, 0);
+    });
 });
-
-// var interval = $('#latest-image').data('interval');
-// window.setInterval(function() {
-//   var latestImage = 'latest/image.jpg';
-//   var jqxhr = $.get(latestImage).done(function() {
-//     $('#latest-image').attr('src', latestImage);
-//   });
-// }, interval);
