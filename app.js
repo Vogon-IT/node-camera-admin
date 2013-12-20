3c99ceb2b2c739b7608793c55003a7915c5ea2b2
teemujonkkari <teemu.jonkkari@gmail.com>
2013-12-20 22:08:33 +0200

    Ugly fix

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ var configPath = 'NodeMap.pfs',
 
 // Config values from database
 var db = new sqlite3.Database(dbPath);
+
 db.serialize(function() {
   db.each("SELECT * FROM CONFIG", function(err, row) {
     if (row.property.match(/Interval/)) photoInterval = parseInt(row.value, 10);
@@ -26,164 +27,168 @@ db.serialize(function() {
 });
 db.close();
 
-fs.exists(configPath, function(exists) {
-  if (!exists) return console.log('ERROR! Config file not found.');
-});
+setTimeout(function() { // wait until db values are loaded. refactor with promises
 
-fs.exists(dbPath, function(exists) {
-  if (!exists) return console.log('ERROR! SQLite database not found.');
-});
-
-fs.exists(backupPath, function(exists) {
-  if (!exists) fs.mkdirSync(backupPath);
-});
-
-fs.watch(imageFolder, function(event, filename) {
-  var imagePath = imageFolder + filename;
-
-  if (filename.match(/(jpg|jpeg)$/)) {
-    fs.exists(imagePath, function(exists) {
-      if (exists) {
-        fs.readFile(imagePath, function(err, data) {
-          if (!err) {
-
-            // https://github.com/rsms/node-imagemagick
-            im.resize({
-              srcPath: imageFolder + filename,
-              dstPath: 'public/' + latestImageFolder + '/image.jpg',
-              width: 860
-            }, function(err, stdout, stderr) {
-              if (err) util.puts(err);
-            });
-
-            //  fs.createReadStream(imageFolder + filename)
-            //    .pipe(fs.createWriteStream('public/' + latestImageFolder + '/image.jpg'));
-
-          } else util.puts(err);
-        });
-      }
-    });
-  }
-});
-
-var options = {
-  views: {
-    path: 'templates',
-    engines: {
-      html: 'handlebars'
-    }
-  }
-};
-
-// Create a server with a host, port, and options
-var server = Hapi.createServer('87.94.74.47', 8080, options);
-
-server.route({
-  method: 'GET',
-  path: '/{path*}',
-  handler: {
-    directory: {
-      path: './public',
-      listing: false,
-      index: true
-    }
-  }
-});
-
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: imageIndex
-});
-
-server.route({
-  method: 'GET',
-  path: '/admin',
-  handler: adminIndex
-});
-
-server.route({
-  method: 'POST',
-  path: '/admin',
-  handler: formHandler
-});
-
-// Start the server
-server.start(function() {
-  util.puts('> Server started at ' + server.info.uri);
-});
-
-function imageIndex(request) {
-  fs.stat('public/' + latestImageFolder + '/image.jpg', function(err, stats) {
-    var modified = moment(stats.mtime).fromNow();
-
-    // Render the view
-    request.reply.view('index.html', {
-      image: latestImageFolder + '/image.jpg',
-      modified: modified,
-      interval: photoInterval
-    });
-
+  fs.exists(configPath, function(exists) {
+    if (!exists) return console.log('ERROR! Config file not found.');
   });
-}
 
-function adminIndex(request) {
-  // Get config file data
-  var configData = fs.readFileSync(configPath).toString();
+  fs.exists(dbPath, function(exists) {
+    if (!exists) return console.log('ERROR! SQLite database not found.');
+  });
 
-  // https://github.com/mapbox/node-sqlite3/wiki/API
-  var db = new sqlite3.Database(dbPath);
+  fs.exists(backupPath, function(exists) {
+    if (!exists) fs.mkdirSync(backupPath);
+  });
 
-  db.serialize(function() {
-    db.all("SELECT * FROM CONFIG", function(err, rows) {
+  fs.watch(imageFolder, function(event, filename) {
+    var imagePath = imageFolder + filename;
+    console.log(imagePath);
+    if (filename.match(/(jpg|jpeg)$/)) {
+      fs.exists(imagePath, function(exists) {
+        if (exists) {
+          fs.readFile(imagePath, function(err, data) {
+            if (!err) {
+
+              // https://github.com/rsms/node-imagemagick
+              im.resize({
+                srcPath: imageFolder + filename,
+                dstPath: 'public/' + latestImageFolder + '/image.jpg',
+                width: 860
+              }, function(err, stdout, stderr) {
+                if (err) util.puts(err);
+              });
+
+              //  fs.createReadStream(imageFolder + filename)
+              //    .pipe(fs.createWriteStream('public/' + latestImageFolder + '/image.jpg'));
+
+            } else util.puts(err);
+          });
+        }
+      });
+    }
+  });
+
+  var options = {
+    views: {
+      path: 'templates',
+      engines: {
+        html: 'handlebars'
+      }
+    }
+  };
+
+  // Create a server with a host, port, and options
+  var server = Hapi.createServer('87.94.74.47', 8080, options);
+
+  server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: {
+      directory: {
+        path: './public',
+        listing: false,
+        index: true
+      }
+    }
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: imageIndex
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/admin',
+    handler: adminIndex
+  });
+
+  server.route({
+    method: 'POST',
+    path: '/admin',
+    handler: formHandler
+  });
+
+  // Start the server
+  server.start(function() {
+    util.puts('> Server started at ' + server.info.uri);
+  });
+
+  function imageIndex(request) {
+    fs.stat('public/' + latestImageFolder + '/image.jpg', function(err, stats) {
+      var modified = moment(stats.mtime).fromNow();
+
       // Render the view
-      request.reply.view('admin.html', {
-        configData: configData,
-        dbData: rows
+      request.reply.view('index.html', {
+        image: latestImageFolder + '/image.jpg',
+        modified: modified,
+        interval: photoInterval
       });
+
     });
-  });
-  db.close();
+  }
 
-}
+  function adminIndex(request) {
+    // Get config file data
+    var configData = fs.readFileSync(configPath).toString();
 
-function formHandler(request) {
-  var payload = request.payload;
-  var db = new sqlite3.Database(dbPath);
+    // https://github.com/mapbox/node-sqlite3/wiki/API
+    var db = new sqlite3.Database(dbPath);
 
-  if (parseInt(payload.interval, 10) > 0) photoInterval = parseInt(payload.interval, 10);
-
-  db.parallelize(function() {
-    var stmt = db.prepare("UPDATE CONFIG SET value = ? WHERE property = ?");
-
-    for (var property in payload) {
-      if (property !== 'configData') {
-        stmt.run(payload[property], property);
-      }
-    }
-
-    stmt.finalize();
-  });
-
-  db.close();
-
-  // ConfigData
-  var configData = payload.configData;
-  // Make backup file from old config file
-  fs.readFile(configPath, function(err, data) {
-    fs.writeFile(backupPath + '/Backup ' + new Date(), data, function(err) {
-      // Save new configuration after backup is done
-      fs.writeFile(configPath, configData, function(err) {
-        if (err) return request.reply({
-          status: 0,
-          message: err.message
+    db.serialize(function() {
+      db.all("SELECT * FROM CONFIG", function(err, rows) {
+        // Render the view
+        request.reply.view('admin.html', {
+          configData: configData,
+          dbData: rows
         });
       });
     });
-  });
+    db.close();
 
-  request.reply({
-    status: 1,
-    message: ''
-  });
-}
+  }
+
+  function formHandler(request) {
+    var payload = request.payload;
+    var db = new sqlite3.Database(dbPath);
+
+    if (parseInt(payload.interval, 10) > 0) photoInterval = parseInt(payload.interval, 10);
+
+    db.parallelize(function() {
+      var stmt = db.prepare("UPDATE CONFIG SET value = ? WHERE property = ?");
+
+      for (var property in payload) {
+        if (property !== 'configData') {
+          stmt.run(payload[property], property);
+        }
+      }
+
+      stmt.finalize();
+    });
+
+    db.close();
+
+    // ConfigData
+    var configData = payload.configData;
+    // Make backup file from old config file
+    fs.readFile(configPath, function(err, data) {
+      fs.writeFile(backupPath + '/Backup ' + new Date(), data, function(err) {
+        // Save new configuration after backup is done
+        fs.writeFile(configPath, configData, function(err) {
+          if (err) return request.reply({
+            status: 0,
+            message: err.message
+          });
+        });
+      });
+    });
+
+    request.reply({
+      status: 1,
+      message: ''
+    });
+  }
+
+}, 1000);
